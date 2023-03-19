061d088d50e6048222c09c4790c62e23afb03515
RicardoJun10r <ricardojunior0110@gmail.com>
2023-03-19 13:07:31 -0300

    Acrescentando HashMap e função que retorna todos os dados de eficiência de cada equipamento

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, add_localizacao, add_eficiencia, listar_eficiencia, get_localizacao, deletar_localizacao
+from .views import home, mapa, dados, add_localizacao, add_eficiencia, listar_eficiencia, get_localizacao, deletar_localizacao
 
 urlpatterns = [
     path('', home),
+    path('heatmap/', dados, name='heat_map'),
+    path('mapa/<int:loc_id>/', mapa, name='mostrar_mapa'),
     path('add_loc/', add_localizacao, name='add_localizacao'),
     path('add_efi/<int:loc_id>/', add_eficiencia, name='add_eficiencia'),
     path('list_efi/', listar_eficiencia, name='list_eficiencia'),
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Localizacao, Eficiencia
+from .utils.hashMap import HashTable
 
 # Create your views here.
 def home(request):
     localizacao = listar_localizacao(request)
     return render(request, "tarefas.html",{"localizacao": localizacao})
+
+def dados(request):
+    
+    IDs = []
+    equipamentos = Localizacao.objects.all().values_list()
+    cont = 0
+
+    # Pegando os IDS
+    for cont in range(equipamentos.__len__()):
+        IDs.append(equipamentos[cont][0])
+        cont = cont + 1
+
+    # Pegando a eficiência de cada equipamento
+    hMap = HashTable(IDs.__len__()-1)
+    i = 0
+    for i in range(IDs.__len__()):
+        hMap.set_val(IDs[i], Eficiencia.objects.filter(localizacao_id=IDs[i]).values_list())
+        i = i + 1
+    print(hMap)
+    return render(request, "heat_map.html")
+
+def mapa(request, loc_id):
+    localizacao = Localizacao.objects.get(id=loc_id)
+    return render(request, "mapbox.html", {"localizacao":localizacao})
 
 def add_localizacao(request):
     nova_longitude = request.POST['longitude']
