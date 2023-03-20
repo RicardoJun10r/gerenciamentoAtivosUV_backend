5ed6d008b4374990f84b9ac62e4d5294fedfd8ff
RicardoJun10r <ricardojunior0110@gmail.com>
2023-03-20 16:55:35 -0300

    ultimas att

--- gerenciamento_ativos/settings.py
+++ gerenciamento_ativos/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ativos_db',
-        'USER': 'postgres',
+        'USER': 'admin',
         'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
